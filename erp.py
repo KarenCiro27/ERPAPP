d107d08b6e853d0392b4a070e996c67c30679b4c
KarenCiro27 <Karenciro274047@correo.itm.edu.co>
2024-11-27 23:43:22 -0500

    Update erp.py

--- erp.py
+++ erp.py
@@ -21,8 +21,8 @@ st.write("Bienvenido al sistema ERP para la gestión de clientes, inventarios, f
 st.sidebar.title("ERP_ITM")
 
 # Variables de autenticación
-USER = "Vale"
-PASSWORD = "Vale03"
+USER = "Karen"
+PASSWORD = "Karenciro2"
 
 # Inicialización de variables globales
 if "auth" not in st.session_state:
